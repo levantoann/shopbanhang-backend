0f3d4b73480b841e2c0dc8137ca424789b62b483
levantoann <lvtoan.19it4@vku.udn.vn>
2023-08-15 01:12:00 +0700

    update2

--- src/routes/index.js
+++ src/routes/index.js
@@ -4,10 +4,10 @@ const OrderRouter = require('./OrderRouter')
 const PaymentRouter = require('./PaymentRouter')
 
 const routes = (app) => {
-    app.use('/api/user',UserRouter) 
-    app.use('/api/product',ProductRouter) 
-    app.use('/api/order',OrderRouter)
-    app.use('/api/payment', PaymentRouter) 
+    app.use('/api/user', UserRouter)
+    app.use('/api/product', ProductRouter)
+    app.use('/api/order', OrderRouter)
+    app.use('/api/payment', PaymentRouter)
 }
 
 module.exports = routes
